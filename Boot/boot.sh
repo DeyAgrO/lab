d338b25ee4f6a8750f913b4cb4d74b45c5a7ebde
DeyAgrO <134607728+DeyAgrO@users.noreply.github.com>
2024-06-30 17:52:42 +0200

    Update To Fit the Python Libraries boot.sh

--- Boot/boot.sh
+++ Boot/boot.sh
@@ -7,7 +7,11 @@ sudo dnf update -y
 sudo dnf upgrade -y
 
 # Install the packages
-sudo dnf install vim-enhanced firewalld net-tools qemu-guest-agent openssh-server git curl wget -y
+sudo dnf install vim-enhanced firewalld net-tools qemu-guest-agent openssh-server git curl wget python3 pip -y
+
+# install needed python Models
+pip install psutil
+pip install termcolor
 
 # Disable SELinux
 sudo sed -i 's/^SELINUX=.*/SELINUX=disabled/' /etc/selinux/config
@@ -16,6 +20,6 @@ echo "Packages installed successfully!"
 echo "SELinux has been disabled."
 
 # Reboot the system
-echo "Rebooting the system in 20 seconds..."
+echo "Powering Off the system in 20 seconds..."
 sleep 20
-sudo reboot
+sudo poweroff
